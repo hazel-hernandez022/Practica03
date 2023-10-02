5a448a296a594592db5c685ff269f8d22169cf3e
hazel-hernandez022 <hazel.perez79@unach.mx>
2023-10-02 14:17:26 -0500

    Agregar archivos de proyecto.

---
+++ Practica03/Practica03.py
@@ -0,0 +1,14 @@
+numeros = [10, 20, 30, 40, 50, 60]
+print(numeros)
+print(numeros[1])
+n = [3, 4, 5]
+print(n)
+print(n[2])
+c = ['L', 'u', 'i', 's']
+print(c)
+print(c[0])
+print(c[1])
+s = "Mona Lisa"
+print(s)
+print(type (2))
+print(s[4])
